2f8b2e7228dcee0d905d80d570e7f42eb551a437
Ryguy <118482718+Ryguy0777@users.noreply.github.com>
2026-06-08 16:58:50 -0500

    Update NSMBW-Updated

--- PropelParts/include/propelparts/nsmbw-updated/nsmbwup_user_config.h
+++ PropelParts/include/propelparts/nsmbw-updated/nsmbwup_user_config.h
@@ -42,3 +42,14 @@
 // file containing that particular bugfix, to indicate that it should be
 // compiled separately for different versions (once for E2, and once for
 // everything else).
+
+#include <propelparts/game_config.h>
+
+// Remove the NSMBW Updated fixes that change some levels
+#ifdef NO_INTRUSIVE_NSMBWUP_FIXES
+
+#define NSMBWUP_C00002_OFF
+
+#define NSMBWUP_C00003_OFF
+
+#endif
